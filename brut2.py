73ba01acaa17bddb7b0581e8861caca3a43f400a
6eth <6ethereum@gmail.com>
2023-04-09 22:55:57 -0400

    nn
    jm

--- brut2.py
+++ brut2.py
@@ -8,7 +8,7 @@ from  etherscan import Etherscan
 import time
 import os
 
-keyli = ["xxxxxxxxxxxxxxx"]
+keyli = ["RVDA9SRNDTTRX4DW8EPEEIASRINBBVB5WU"]
 
 def readdr():    
     MNEMONIC: str = generate_mnemonic(language="english", strength=128)
